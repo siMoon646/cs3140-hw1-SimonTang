084ee19f16c1a945f4e7d4f4f238c77ee344cce5
siMoon646 <simontang671@gmail.com>
2026-01-29 21:48:15 -0500

    feat: add countVowels

--- index.js
+++ index.js
@@ -43,4 +43,14 @@ function reverse(str){
     return s;
 }
 
-console.log(reverse("hello"));
+function countVowels(str){
+    let counter = 0;
+    for (let i = 0; i < str.length; i++){
+        if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u'){
+            counter++;
+        }
+    }
+    return counter;
+}
+
+console.log(countVowels("hello")); // 2
